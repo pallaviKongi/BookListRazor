c1010e93d84c95f99ea39aea114d009dd68e6258
Pallavi Kongi <48719413+pallaviKongi@users.noreply.github.com>
2021-10-16 16:40:42 +0700

    Develop (#1)
    * Book List razor app
    
    * Space change
    
    Co-authored-by: Kongi, Pallavi (Agoda) <pallavi.kongi@agoda.com>

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Pass the options to the base class
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
